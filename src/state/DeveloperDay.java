159c75cdf8c1a5902404c9f67b4ea7703b7f3be0
Nodira Rakhimjonova <rahimjonovanodira@gmail.com>
2019-02-28 15:25:13 +0500

    First

---
+++ src/state/DeveloperDay.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package state;
+
+/**
+ *
+ * @author nodira
+ */
+public class DeveloperDay {
+
+   
+    public static void main(String[] args) {
+        Activity activity = new Sleeping();
+        Developer developer = new Developer();
+        
+        developer.setActivity(activity);
+        
+        for(int i=0; i<10; i++){
+            developer.justDoIt();
+            developer.changeActivity();
+        }
+    }
+    
+}
